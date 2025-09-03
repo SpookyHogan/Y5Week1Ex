9ec00c474a8c78170c28cec179cd7563eb4ec132
Samuel Hogan <hoga0129@algonquinlive.com>
2025-09-03 13:09:54 -0400

    tested the nature of char

--- DuplicateDestroyer/main.cpp
+++ DuplicateDestroyer/main.cpp
@@ -4,10 +4,36 @@
 using namespace std;
 
 int main()
-{
+{ // 3vo
+
+    //char* bob = new char[5] {'h', 'e', 'l', 'l', 'o'};
+
+    //char bob1 = bob[0];
+    //char bob2 = bob[1];
+    //char bob3 = bob[2];
+    //char bob4 = bob[3];
+    //char bob5 = bob[4];
+
+
+
+    //*bob = *bob - 'l';
+
+    //char ibob1 = bob[0];
+    //char ibob2 = bob[1];
+    //char ibob3 = bob[2];
+    //char ibob4 = bob[3];
+    //char ibob5 = bob[4];
+
+    //if (bob)
+    //{
+    //    delete[] bob;
+    //}
+
+
     cout << "Please enter your text and I, in my infinite wisdom, will remove any duplicate characters\n";
 
-    string enteredText; // TODO: figure out how to get multiple words
+    string enteredText;
+    // TODO: figure out how to get multiple words
 
     //getline(std::cin, enteredText);
 
@@ -17,7 +43,7 @@ int main()
 
     int length2 = 0;
 
-    char index = 'q';
+    char index = 'q'; // this does not matter, I just need to initialize it to something other than '\0'
 
     do
     {
@@ -41,7 +67,7 @@ int main()
             }
             j++;
         }
-        //if (enteredText[i] & usedCharacters[j])
+        //if (enteredText & string(usedCharacters))
         //{
         //    isUnique = false;
         //}
